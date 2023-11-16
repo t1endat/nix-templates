e769f16fca90aa42f4c2cbf79517c57122c7b766
tiendatAI <tiendatlework@gmail.com>
2023-11-16 13:49:03 +0700

    add evn for jupyter

---
+++ jupyter/flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (python3.withPackages(ps: with ps; [
+            pip #for install extensions
+            ipython
+            jupyterlab
+
+            #additional packages
+            numpy
+            seaborn
+            pandas
+            matplotlib
+            scikit-learn
+          ]))
+        ];
+        shellHook = "jupyter-lab";
+      };
+    }
+  );
+}
